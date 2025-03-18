897edcfbb90c28b7ed9d3af408d9dac90b24a997
juanpimedicen <53624464+juanpimedicen@users.noreply.github.com>
2025-03-18 19:53:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosLista = []; //Lista para los nombres de amigos a introducir
 
-function asignarTextoAmigo (elemento, amigo){
-let elementoHTML= document.querySelector(elemento);
-elementoHTML.innerHTML = amigoAgregado;
+function asignarTextoAmigo (){
+    //lista del HTML
+    let listaMostrar = document.querySelector("#listaAmigos");
+    listaMostrar.innerHTML = ""; //Primero la dejamos en blanco
+
+    //Se recorre la lista para agregar cada uno como elemnto dentro de <li>
+    amigosLista.forEach(function(amigo) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaMostrar.appendChild(li);
+    });
 
 }
 
 function agregarAmigo(){
-let amigoAgregado = document.getElementById('amigo').value;
-
-console.log(amigoAgregado);
-limpiarInput();
-return;
-
-}
-
-function sortearAmigo(){
-
+    //Se toma lo que introduce el usuario
+    let input = document.getElementById("amigo");
+    let nombreInput = input.value.trim();
+    //Se valida que no esté vacío
+    if (nombreInput === ""){
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+     amigosLista.push(nombreInput) //Se inserta el nombre en la lista creada
+    //Se escribe en la página los nombres
+    asignarTextoAmigo();
+    
+    //Se limpia la caja de entrada de dato
+    limpiarInput();
+    return;
 
 }
 //Agregando la función de limpiar la caja
 function limpiarInput() {
     document.querySelector('#amigo').value = '';
 }
-agregarAmigo();
-asignarTextoAmigo('ul',amigoAgregado);
+function sortearAmigo(){
+    //Se valida si se encuentra vacío para hacer sorteo
+    if (amigosLista.length == 0){
+        alert("No hay amigos en la lista para hacer el sorteo");
+        return;
+    }
+    //Se limpia lista de amigos en la pantalla
+    document.getElementById("listaAmigos").innerHTML = '';
+    //Se genera un numero random para elegir amigo
+    let randomNum = Math.floor(Math.random()*amigosLista.length);
+    //Se selecciona el amigo
+    let nombreAmigo=amigosLista[randomNum];
+    //Se escribe en la página el resultado
+    let resultado =document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 El amigo sorteado es: <strong>${nombreAmigo}</strong> 🎉</li>`;
+}
+
+//Agregando la función de limpiar la caja
+function limpiarInput() {
+    document.querySelector('#amigo').value = '';
+}
