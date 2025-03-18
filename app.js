cf41b277a49f5ffd3c4b26bc9c9f8e8e3d868381
juanpimedicen <53624464+juanpimedicen@users.noreply.github.com>
2025-03-18 07:46:23 -0400

    Update in git app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function asignarTextoAmigo (elemento, amigo){
+let elementoHTML= document.querySelector(elemento);
+elementoHTML.innerHTML = amigoAgregado;
+
+}
+
+function agregarAmigo(){
+let amigoAgregado = document.getElementById('amigo').value;
+
+console.log(amigoAgregado);
+return;
+
+}
+
+function sortearAmigo(){
+
+
+}
+
+agregarAmigo();
+asignarTextoAmigo('ul',amigoAgregado);
